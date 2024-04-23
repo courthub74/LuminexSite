5c4cc18e66b7f1e61188efecf9ff98e18c440cdd
Courdevelops <courdevelops@gmail.com>
2024-04-23 13:58:11 -0400

    two boxes done

--- boxes.js
+++ boxes.js
@@ -4,11 +4,15 @@
 
 // each button
 let button_one = document.getElementById('button_one');
+let button_two = document.getElementById('button_two');
 
 // each paragraph
 let box_one = document.getElementById('box_one_words');
+let box_two = document.getElementById('box_two_words');
 
 // add event listener to the button
+
+// One
 // to mouseover hover
 button_one.addEventListener('mouseover', () => {
     // change the style of the words
@@ -18,4 +22,16 @@ button_one.addEventListener('mouseover', () => {
 // to mouseout take mouse away
 button_one.addEventListener('mouseout', () => {
     box_one.classList.remove('appear');
-})
+});
+
+// Two
+// to mouseover hover
+button_two.addEventListener('mouseover', () => {
+    // change the style of the words
+    box_two.classList.add('appear');
+});
+
+// to mouseout take mouse away
+button_two.addEventListener('mouseout', () => {
+    box_two.classList.remove('appear');
+});
